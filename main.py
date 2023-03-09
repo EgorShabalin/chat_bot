008c45c7c991e91bc5b205fee96dad88504e475c
egor <egor.shabalin.wd@gmail.com>
2023-03-09 23:25:32 +0300

    buttons added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import redis
 from config import TG_KEY, GPT_KEY
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
 
 
 openai.api_key = GPT_KEY
@@ -11,21 +13,30 @@ openai.api_key = GPT_KEY
 bot = Bot(token=TG_KEY)
 dp = Dispatcher(bot)
 
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+start_button = KeyboardButton(text='/start')
+del_button = KeyboardButton(text='/del')
+
+kb.add(start_button, del_button)
+
 rdb = redis.Redis(host='redis', port=6379, db=0)
+
 
 @dp.message_handler(commands=['start'])
 async def start_func(message: types.Message):
     print('Bot has started!')
-    await message.reply('Welcome to Chat_bot. This bot uses ChatGPT!\n\nTo clear cache use command /del.')
+    await message.reply('Welcome to Chat_bot. This bot uses ChatGPT!\n\nTo clear cache use command /del.',
+                        reply_markup=kb)
+
 
 @dp.message_handler(commands=['del'])
 async def clear_cache(message: types.Message):
     user_id = types.User.get_current().id
     rdb.delete(user_id)
     rdb.close()
-    await message.reply('Chache was cleared!')
+    await message.reply('Chache was cleared!',
+                        reply_markup=kb)
     
-
 
 @dp.message_handler()
 async def get_message(message: types.Message)->None:
@@ -55,7 +66,6 @@ async def get_message(message: types.Message)->None:
     )
 
     answer = response['choices'][0]['text']
-
     await message.reply(f'<code>{answer}</code>', parse_mode='html')
 
     chat_history = chat_history + answer
