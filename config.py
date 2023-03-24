182786a04a9b7fc96a09c03c79ab9ac28dbbe62b
egor <egor.shabalin.wd@gmail.com>
2023-03-24 18:53:47 +0300

    secrets handling

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 import dotenv
 
 
-dotenv.load_dotenv('.env')
+# dotenv.load_dotenv('.env')
 
-TG_KEY = os.environ['TG_KEY']
+TG_KEY = os.environ["TG_KEY"]
 
-GPT_KEY = os.environ['GPT_KEY']
+GPT_KEY = os.environ["GPT_KEY"]
